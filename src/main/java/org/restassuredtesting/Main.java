12352ebb33fb7deb3141be58c7643ef31b505c73
meldanurcelik <melda_celik@hotmail.com>
2022-12-28 17:01:26 +0300

    Integrating the Multiple API's with common Json response values

--- src/main/java/org/restassuredtesting/Main.java
+++ src/main/java/org/restassuredtesting/Main.java
@@ -32,5 +32,17 @@ public class Main {
         String placeId = js.getString("place_id");
         System.out.println(placeId);
 
+        //Update place
+        given().log().all().queryParam("key", "qaclick123")
+                .header("Content-Type", "application/json")
+                .body("{\n" +
+                        "\"place_id\":\"" + placeId + "\",\n" +
+                        "\"address\":\"70 Summer walk, USA\",\n" +
+                        "\"key\":\"qaclick123\"\n" +
+                        "}\n")
+                .when().put("/maps/api/place/update/json")
+                .then().assertThat().log().all().statusCode(200)
+                .body("msg", equalTo("Address successfully updated"));
+
     }
 }
