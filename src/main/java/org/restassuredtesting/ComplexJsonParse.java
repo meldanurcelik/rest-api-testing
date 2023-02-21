498cd0bb629219c63017b15fce62bb77b33a704f
meldanurcelik <melda_celik@hotmail.com>
2023-02-21 20:53:48 +0300

    Iterating over every element of Json Array and accessing elements in it

--- src/main/java/org/restassuredtesting/ComplexJsonParse.java
+++ src/main/java/org/restassuredtesting/ComplexJsonParse.java
@@ -20,6 +20,13 @@ public class ComplexJsonParse {
         String titleFirstCourse = js.get("courses[2].title");
         System.out.println("courses[2].title = " + titleFirstCourse);
 
+        //Print All course titles and their respective Prices
+        for (int i = 0; i < count; i++) {
+            String courseTitles = js.get("courses[" + i + "].title");
+            System.out.println(js.get("courses[" + i + "].price").toString());
+            System.out.println(courseTitles);
+        }
+
     }
 
 }
