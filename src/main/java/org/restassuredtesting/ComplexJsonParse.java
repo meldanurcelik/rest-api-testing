28f726defb90468c20c6e74624ab3074903c36ce
meldanurcelik <melda_celik@hotmail.com>
2023-02-21 20:51:12 +0300

    Retrieving the Json Array Size and its elements using JsonPath

--- src/main/java/org/restassuredtesting/ComplexJsonParse.java
+++ src/main/java/org/restassuredtesting/ComplexJsonParse.java
@@ -8,6 +8,18 @@ public class ComplexJsonParse {
 
         JsonPath js = new JsonPath(Payload.CoursePrice());
 
+        //Print No of courses returned by API
+        int count = js.getInt("courses.size()");
+        System.out.println("courses.size() = " + count);
+
+        //Print Purchase Amount
+        int totalAmount = js.getInt("dashboard.purchaseAmount");
+        System.out.println("dashboard.purchaseAmount = " + totalAmount);
+
+        //Print Title of the first course
+        String titleFirstCourse = js.get("courses[2].title");
+        System.out.println("courses[2].title = " + titleFirstCourse);
+
     }
 
 }
