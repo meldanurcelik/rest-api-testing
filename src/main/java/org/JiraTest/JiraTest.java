c26ebadb41f7c682a668d1cb59b0e55809fd680a
meldanurcelik <melda_celik@hotmail.com>
2023-02-24 17:54:59 +0300

    Importance of Session Filter cookie in Rest Assured Code

--- src/main/java/org/JiraTest/JiraTest.java
+++ src/main/java/org/JiraTest/JiraTest.java
@@ -1,6 +1,7 @@
 package org.JiraTest;
 
 import io.restassured.RestAssured;
+import io.restassured.filter.session.SessionFilter;
 
 import static io.restassured.RestAssured.*;
 
@@ -10,9 +11,22 @@ public class JiraTest {
 
         RestAssured.baseURI = "http://localhost:8080";
 
-        given().pathParam("id", "10001")
+        SessionFilter sessionFilter = new SessionFilter();
+
+        String response = given()
                 .header("Content-type", "application/json")
+                .body("{\n" +
+                        "    \"username\": \"meldanur.celik\",\n" +
+                        "    \"password\": \"Gini123\"\n" +
+                        "}")
                 .log().all()
+                .filter(sessionFilter).when()
+                .post("/rest/auth/1/session")
+                .then().log().all().extract().response().asString();
+
+        given().pathParam("key", "10104")
+                .log().all()
+                .header("Content-type", "application/json")
                 .body("{\n" +
                         "    \"body\": \"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque eget venenatis elit. Duis eu justo eget augue iaculis fermentum. Sed semper quam laoreet nisi egestas at posuere augue semper\",\n" +
                         "    \"visibility\": {\n" +
@@ -20,7 +34,8 @@ public class JiraTest {
                         "        \"value\": \"Administrators\"\n" +
                         "    }\n" +
                         "}")
-                .post("/rest/api/2/issue/{id}/comment");
+                .filter(sessionFilter).when()
+                .post("/rest/api/2/issue/{key}/comment").then().assertThat().statusCode(201);
 
     }
 
